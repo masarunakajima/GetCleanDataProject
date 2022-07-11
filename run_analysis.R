c8775e530261d247c5b95f37ddd9f01482c289d3
masarunakajima <masarun@usc.edu>
2022-07-10 21:34:28 -0700

    First commit

---
+++ run_analysis.R
@@ -0,0 +1,39 @@
+#Assume that the working directory contains the directory 'UCI HAR Dataset'
+library(dplyr)
+
+# Get features
+features <- read.table("UCI HAR Dataset/features.txt")
+features_vec <- features$V2
+
+# read the train and test files (4. get variable names)
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features_vec)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = c("Activity"))
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features_vec)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = c("Activity"))
+
+
+#1. Merges the training and the test sets to create one data set.
+x <- rbind(x_train, x_test)
+y <- rbind(y_train, y_test)
+
+#2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+mask <- grepl("mean", features_vec)|grepl("std", features_vec)
+mean_std <- x[,mask]
+
+#3. Uses descriptive activity names to name the activities in the data set
+act_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+act_map <- factor(act_labels$V2)
+y <- mutate(y, label=act_map[Activity])
+#x$label <- y$label
+
+#4. Done earlier
+
+#5. From the data set in step 4, creates a second, independent tidy data set 
+#   with the average of each variable for each activity and each subject.
+sub_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+sub_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+sub <- rbind(sub_train, sub_test)
+Subject <- sub$V1
+Activity <- y$label
+act_sub_mean <- aggregate(.~Activity+Subject, x, mean)
+
